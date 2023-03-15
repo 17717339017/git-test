230ed9a30f1ce05253503977b2794bd0e5faf264
wangwei <17717339017@163.com>
2023-03-15 18:41:28 +0800

    台式 ts

--- test.php
+++ test.php
@@ -1,2 +1,28 @@
 <?php
+    interface school{
+        public function name();
+        public function age();
+    }
 
+    class teacher implements school {
+        public  $name = "张三";
+        public $age = 25;
+
+        public function __construct($name , $age) {
+            $this->name = $name;
+            $this->age = $age;
+        }
+
+        public function name() {
+            return $this->name = 1;
+        }
+        public function age(){
+            return $this->age = 2;
+        }
+        public function info() {
+            return "我叫". $this->name() . "我" . $this->age();
+        }
+    }
+
+    $info = new teacher("王五", 35);
+    echo $info->info();
